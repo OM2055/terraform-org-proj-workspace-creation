4888bbeaef4c7741f5df5f70ccee39d72591d1a4
OM2055 <omkar.dba@gmail.com>
2024-09-05 14:15:10 +0530

    Update main.tf

--- modules/projects/main.tf
+++ modules/projects/main.tf
@@ -3,15 +3,27 @@ resource "tfe_project" "project" {
   organization   = var.organization_id
 }
 
-# Create workspaces in the project
-module "workspace_1" {
-  source       = "../workspaces"
-  workspace_name  = "${var.project_name}-workspace-1"
-  project_id      = tfe_project.project.id
+# Create Projects
+module "project_1" {
+  source           = "./modules/projects"
+  project_name     = var.project_1_name
+  organization_id  = data.tfe_organization.existing_org.id
 }
 
-module "workspace_2" {
-  source       = "../workspaces"
-  workspace_name  = "${var.project_name}-workspace-2"
-  project_id      = tfe_project.project.id
+module "project_2" {
+  source           = "./modules/projects"
+  project_name     = var.project_2_name
+  organization_id  = data.tfe_organization.existing_org.id
 }
+# Create workspaces in the project
+# module "workspace_1" {
+#  source       = "../workspaces"
+#  workspace_name  = "${var.project_name}-workspace-1"
+#  project_id      = tfe_project.project.id
+#}
+
+#module "workspace_2" {
+#  source       = "../workspaces"
+#  workspace_name  = "${var.project_name}-workspace-2"
+#  project_id      = tfe_project.project.id
+#}
